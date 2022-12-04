d739bd14d3f3636c4fa42b57336fd2252eb4a496
+ <kelsey.hurley@flatironschool.com>
2022-12-04 17:06:16 -0500

    commit tree 0afb2aada3307a06f49e26353151f74cf3a17b07

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Follow along with the examples here
+console.log("Hello, world!");
+function sayHello() {
+    console.log("Hello!");
+  }
+
+  sayHello();
+  sayHello();
+  sayHello();
+  function sayHelloToGuadalupe() {
+  console.log("Hello, Guadalupe!");}
+  sayHelloToGuadalupe()
+  
